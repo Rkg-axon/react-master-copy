5f3d620cf1609f25779f0cfc35d57aa78d1e91d1
pukhrajp <pukhraj.prajapat@g-axon.com>
2024-03-05 15:00:28 +0530

    - update div

--- src/@jumbo/shared/components/Div/Div.tsx
+++ src/@jumbo/shared/components/Div/Div.tsx
@@ -1,11 +1,6 @@
 'use client';
 import styled from '@mui/material/styles/styled';
 
-const Div = styled('div')({
-  color: 'darkslategray',
-  backgroundColor: 'aliceblue',
-  padding: 8,
-  borderRadius: 4,
-});
+const Div = styled('div')({});
 
 export { Div };
