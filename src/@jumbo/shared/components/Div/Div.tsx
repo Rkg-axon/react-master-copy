b4e38b437be4d0487e86efde5185660e30368374
pukhrajp <pukhraj.prajapat@g-axon.com>
2024-03-05 14:52:53 +0530

    latest changes to featured component

--- src/@jumbo/shared/components/Div/Div.tsx
+++ src/@jumbo/shared/components/Div/Div.tsx
@@ -1,6 +1,11 @@
 'use client';
 import styled from '@mui/material/styles/styled';
 
-const Div = styled('div')({});
+const Div = styled('div')({
+  color: 'darkslategray',
+  backgroundColor: 'aliceblue',
+  padding: 8,
+  borderRadius: 4,
+});
 
 export { Div };
