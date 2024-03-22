90c10dc54ff44a75ae7c3037e8690766f2dc295d
Jaikishan Prajapat <jaikishan.prajapat@g-axon.com>
2024-03-22 11:01:50 +0530

    - fix bar chart positive&Negative component

--- src/app/_components/charts/bar/components/PositiveAndNegativeBarChart/PositiveAndNegativeBarChart.tsx
+++ src/app/_components/charts/bar/components/PositiveAndNegativeBarChart/PositiveAndNegativeBarChart.tsx
@@ -11,7 +11,7 @@ import {
   XAxis,
   YAxis,
 } from 'recharts';
-import { barData } from '../..';
+import { posNegData } from '../..';
 
 const PositiveAndNegativeBarChart = () => (
   <JumboCard
@@ -21,33 +21,24 @@ const PositiveAndNegativeBarChart = () => (
   >
     <ResponsiveContainer width='100%' height={200}>
       <BarChart
-        data={barData}
-        margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+        width={500}
+        height={300}
+        data={posNegData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
       >
+        <CartesianGrid strokeDasharray='3 3' />
         <XAxis dataKey='name' />
         <YAxis />
-        <CartesianGrid strokeDasharray='3 3' />
-        <Tooltip
-          labelStyle={{ color: 'black' }}
-          itemStyle={{ color: 'black' }}
-          cursor={false}
-        />
+        <Tooltip />
         <Legend />
-        <defs>
-          <linearGradient id='color1' x1='0' y1='0' x2='0' y2='1'>
-            <stop offset='5%' stopColor='#6200EE' stopOpacity={1} />
-            <stop offset='95%' stopColor='#B819D2' stopOpacity={1} />
-          </linearGradient>
-        </defs>
-        <defs>
-          <linearGradient id='color2' x1='0' y1='0' x2='0' y2='1'>
-            <stop offset='5%' stopColor='#1ABBDE' stopOpacity={1} />
-            <stop offset='95%' stopColor='#09BCA7' stopOpacity={1} />
-          </linearGradient>
-        </defs>
         <ReferenceLine y={0} stroke='#000' />
-        <Bar dataKey='pv' fill={'#1e88e5'} />
-        <Bar dataKey='uv' fill={'#e91e63'} />
+        <Bar dataKey='pv' fill='#1e88e5' />
+        <Bar dataKey='uv' fill='#e91e63' />
       </BarChart>
     </ResponsiveContainer>
   </JumboCard>
